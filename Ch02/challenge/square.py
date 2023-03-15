6b462ea04821a8a1611c56c88ef9b6a60a4a143d
Javier Adanaque <jadanaque@users.noreply.github.com>
2023-03-15 15:06:59 +0000

    solution to img square challenge

--- Ch02/challenge/square.py
+++ Ch02/challenge/square.py
@@ -5,7 +5,7 @@ img = plt.imread('flower.png')
 img = img.copy()  # make img writable
 plt.imshow(img)
 
-#%%
+# %%
 type(img)
 # %%
 img.shape
@@ -15,3 +15,17 @@ img.shape
 # Top-left: 190x350
 # Bottom-right: 680x850
 # Line width: 5
+# %%
+img[350:850, 190:195, 2] = 1
+img[350:850, 675:680, 2] = 1
+img[350:355, 190:680, 2] = 1
+img[845:850, 190:680, 2] = 1
+
+# %% Another Approach
+img[350:850, 190:195] = [0, 0, 1]
+img[350:850, 675:680] = [0, 0, 1]
+img[350:355, 190:680] = [0, 0, 1]
+img[845:850, 190:680] = [0, 0, 1]
+plt.imshow(img)
+
+# %%
