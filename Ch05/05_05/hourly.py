a11d2c7123b1ad94f0cef4cc886abea7fdd82590
Javier Adanaque <jadanaque@users.noreply.github.com>
2023-11-03 21:38:31 +0000

    random changes and tests

--- Ch05/05_05/hourly.py
+++ Ch05/05_05/hourly.py
@@ -26,8 +26,7 @@ df[['day', 'hour']].head()
 # %%
 (
     df.groupby(['Vendor', 'day', 'hour'])
-    .count()
-    ['Vendor']
+    .count()['VendorID']
 )
 
 # %%
--- Ch05/05_06/weather.py
+++ Ch05/05_06/weather.py
@@ -1,4 +1,5 @@
 # %%
+
 import pandas as pd
 
 time_cols = [
